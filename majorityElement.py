9ea2375942ebe3cf345f4f06bbf5b1ac7efdac59
Aschalew Daraje <aschalewdere2@gmail.com>
2026-02-20 07:46:58 -0800

    Create majorityElement.py

---
+++ majorityElement.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        c=Counter(nums)
+        ans=[]
+        for key ,val in c.items():
+            if val>len(nums)/3:
+                ans.append(key)
+        return ans
+        
